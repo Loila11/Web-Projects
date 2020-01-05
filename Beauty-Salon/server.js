791a9a9a02ba79a5338c5681fc2b21626cbc14eb
Loila11 <48774988+Loila11@users.noreply.github.com>
2020-01-05 23:52:00 +0200

    I'm tired

--- Beauty-Salon/server.js
+++ Beauty-Salon/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const uuidv1 = require('uuid');
+const uuidv1 = require('uuid/v1');
 const fs = require("fs");
 const app = express();
 
